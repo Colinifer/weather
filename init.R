8fc1c94bd6adb0e8fd4bb6f85ef3e2d8dc839957
Colin <colinrwelsh@gmail.com>
2020-07-11 00:37:03 -0400

    NOAA updates, api setup

--- init.R
+++ init.R
@@ -1,10 +1,13 @@
 # Packages & Initialize Setup ---------------------------------------------
 
+# Vignettes - https://cran.r-project.org/web/packages/rnoaa/vignettes
+# Install GDAL - https://www.kyngchaos.com/software/frameworks/
+
 pkgs <-
   c("tidyverse",
     "ggplot2",
-    "rnoaa"
-  )
+    "rnoaa",
+    "rgdal")
 
 installed_packages <- pkgs %in%
   rownames(installed.packages())
@@ -12,3 +15,16 @@ if (any(installed_packages == FALSE)) {
   install.packages(pkgs[!installed_packages])
 }
 invisible(lapply(pkgs, library, character.only = TRUE))
+
+
+# API Setup ---------------------------------------------------------------
+
+options(noaakey = "JoGQdxAFzjcEzDNlZluQzfZkHJfMexyE")
+
+# NOAA Functions ----------------------------------------------------------
+
+ncdc_locs(
+  locationcategoryid = 'CITY',
+  sortfield = 'name',
+  sortorder = 'desc'
+)
