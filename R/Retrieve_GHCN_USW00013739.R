4b526c4d04e59c6cd7775a07fabb5bedd475f668
Colin Welsh <colinrwelsh@gmail.com>
2025-05-20 22:26:34 -0400

    updates

--- R/Retrieve_GHCN_USW00013739.R
+++ R/Retrieve_GHCN_USW00013739.R
@@ -1,6 +1,7 @@
 library(readr)
 library(dplyr)
 library(tidyr)
+library(glue)
 
 
 # station readme
@@ -17,39 +18,32 @@ library(tidyr)
 
 # download the zipped file
 station_id <- 'USW00013739' # PHILADELPHIA INTL AP
+data_url <- glue::glue("https://www1.ncdc.noaa.gov/pub/data/ghcn/daily/by_station/{station_id}.csv.gz")
 
-temp <- tempfile()
-download.file(glue::glue("https://www1.ncdc.noaa.gov/pub/data/ghcn/daily/by_station/{station_id}.csv.gz"),temp)
-
-# unzip and read
-ghcn <- read_csv(temp,
-                 col_names = c("id", "yearmoda", "element", "value",
-                               "mflag", "qflag", "sflag", "obs_time"),
-                 col_types = "cccncccc")
-
-# delete the zipped file
-unlink(temp)
-
-ghcn |> 
-  filter(element %in% c("PRCP", "SNOW", "SNWD", "TMAX", "TMIN")) |> 
-  select(element) |> 
-  unique()
+# obtain the updated file
+#   readr::read_csv downloads and unzips .csv.gz
+ghcn <- data.table::fread(data_url,
+                          col.names = c("id", "yearmoda", "element", "value",
+                                        "mflag", "qflag", "sflag", "obs_time"),
+                          colClasses = list(character=c(1:3,5:8),
+                                            numeric=4)) |>
+  as_tibble()
 
 # subset and format
-ghcn.wide <- ghcn %>%
-  select(yearmoda, element, value) %>%
-  filter(element %in% c("PRCP", "SNOW", "SNWD", "TMAX", "TMIN")) %>%
-  separate(col = yearmoda, sep = c(4,6), into = c("year", "month", "day")) %>%
-  pivot_wider(names_from = element, values_from = value) %>%
+ghcn.wide <- ghcn |>
+  select(yearmoda, element, value) |>
+  filter(element %in% c("PRCP", "SNOW", "SNWD", "TMAX", "TMIN")) |>
+  separate(col = yearmoda, sep = c(4,6), into = c("year", "month", "day")) |>
+  pivot_wider(names_from = element, values_from = value) |>
   # convert from tenths of mm to inches
   mutate(PRCP = PRCP * 0.00393701,
          SNOW = SNOW * 0.00393701,
-         SNWD = SNWD * 0.00393701) %>%
+         SNWD = SNWD * 0.00393701) |>
   # convert from tenths of degrees C to F
   mutate(TMAX = ((TMAX / 10) * (9/5)) + 32,
-         TMIN = ((TMIN / 10) * (9/5)) + 32) %>%
+         TMIN = ((TMIN / 10) * (9/5)) + 32) |>
   mutate(date = as.Date(paste(year, month, day, sep = "-")),
-         day_of_year = lubridate::yday(date)) %>%
+         day_of_year = lubridate::yday(date)) |>
   select(year, month, day, date, day_of_year, PRCP, SNOW, SNWD,
          TMAX, TMIN)
 
